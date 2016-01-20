8b3e76fc3f41640f3adff84418de37fa08016629
gkoneru1 <konerugirinandini@gmail.com>
2016-01-19 18:52:01 -0600

    Exercise1-Q12
    Java program to print the table of characters that are equivalent to the
    Ascii codes from 1 to 122.

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+package com.digitek.exercise1;
+
+public class Ascii {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=122;i++){
+			char Ascii= (char)i;
+			System.out.println(Ascii);
+			
+		}
+	}
+
+}
