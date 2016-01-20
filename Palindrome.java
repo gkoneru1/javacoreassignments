dfeaddbb7b845bcad6030fa4a394e30a1840ce10
gkoneru1 <konerugirinandini@gmail.com>
2016-01-19 18:51:42 -0600

    Exercise1-Q11
    Java program to check if a number is palindrome in Java? ( 121 is
    palindrome, 321 is not)

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+package com.digitek.exercise1;
+import java.util.Scanner;
+
+public class Palindrome {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=s.nextInt();
+		int original=n;
+		int reverse=0;
+		
+		while(n!=0){			
+			int r=n%10;
+			reverse=reverse*10+r;
+			n=n/10;
+		}
+		if(original==reverse){
+			System.out.println("Its a palindrome");
+		}
+		else{
+			System.out.println("Its not a palindrome");
+		}
+
+	}
+
+}
+
