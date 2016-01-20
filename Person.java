34a645d897fe47160dc3ecfa9c4b7c6b7b61f1a8
gkoneru1 <konerugirinandini@gmail.com>
2016-01-19 18:45:39 -0600

    Exercise1-Q3
    A method to my Person class called "writeName".This method output the
    text "My name is” followed by the value of the name instance variable.

---
+++ Person.java
@@ -0,0 +1,15 @@
+package com.digitek.exercise1;
+
+public class Person {
+    String name= "GiriNandini";
+	public static void main(String[] args) {
+		 Person p = new Person();
+		 p.writeName();
+
+	}
+	
+	public void writeName(){
+		System.out.println("My Name is:"+ name);
+	}
+
+}
