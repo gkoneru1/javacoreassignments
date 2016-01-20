edfa2e910467f993e7bd7c65761f5da98b403acf
gkoneru1 <konerugirinandini@gmail.com>
2016-01-19 18:50:15 -0600

    Exercise1-Q8
    A program that reads an integer that is typed in by the * user and
    computes and prints the square of that integer.

---
+++ Square.java
@@ -0,0 +1,19 @@
+package com.digitek.exercise1;
+
+import java.util.Scanner;
+
+public class Square {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value:"+" ");
+		int a = s.nextInt();
+		square(a);
+	}
+   public static void square(int b){
+	   int result = (int) Math.pow(b, 2);
+	   System.out.println("The square root of "+ b +"is:"+" "+result);
+   }
+}
+
