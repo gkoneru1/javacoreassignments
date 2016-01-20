315036930783c9abcac2bc4519a1aea02d90ee68
gkoneru1 <konerugirinandini@gmail.com>
2016-01-19 18:48:32 -0600

    Exercise1-Q5
    A Java Program to declare two integer variables , one float variable and
    one string variable and assign 10,12.5 and “Java programming” to them
    respectively .Then displaying their values on the screen.

---
+++ Variables.java
@@ -0,0 +1,13 @@
+package com.digitek.exercise1;
+
+public class Variables {
+	
+	
+	public static void main(String[] args) {
+		int a=10;
+		float b= 12.5f;
+		String c="Java Programming";
+		System.out.println("The value of the integer variable is:"+" " + a +'\n'+ "The value of the float variable is:"+" "+ b+'\n'+ "The value of the string variable is:"+" "+ c);
+	}
+
+}
