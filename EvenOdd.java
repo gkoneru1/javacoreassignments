9fa8d449d69e2649120c267f2abf8c7bc9cfe7d4
gkoneru1 <konerugirinandini@gmail.com>
2016-01-19 18:50:48 -0600

    Exercise1-Q9
    A program in Java to check if a number is even or odd in Java? (input 2
    output true, input 3 : output false)

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+package com.digitek.exercise1;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+		@SuppressWarnings("resource")
+		public static void main(String[] args) {
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter the value:"+" ");
+			int a = s.nextInt();
+			evenOdd(a);
+	}
+   public static void evenOdd(int a){
+	   boolean flag = true;
+	   if(a%2==0){
+		   System.out.println(flag);
+	   }else{
+		   flag=false;
+		   System.out.println(flag);
+	   }
+   }
+}
